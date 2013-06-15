2f9afb6cc1beba5ef9679e57d6de485815f7bacd
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-06-16 01:02:22 +0900

    add python 2 support

--- ci_run.sh
+++ ci_run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # vim:set sts=2 sw=2 tw=0 et:
 
-cd vim && ./configure --with-features=huge \
-  --enable-perlinterp --enable-python3interp \
-  --enable-rubyinterp --enable-luainterp \
+cd vim && ./configure \
+  --with-features=huge \
+  --enable-perlinterp \
+  --enable-pythoninterp \
+  --enable-python3interp \
+  --enable-rubyinterp \
+  --enable-luainterp \
   --enable-fail-if-missing && make && make test
