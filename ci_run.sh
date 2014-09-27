521bb26cd29f91d8178b5ba5549d8922a6c07a1b
K.Takata <kentkt@csc.jp>
2014-09-27 10:07:55 +0900

    Do not enable if_* for small/tiny features

--- ci_run.sh
+++ ci_run.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 # vim:set sts=2 sw=2 tw=0 et:
 
+case $FEATURES in
+  huge | big | normal)
+    opt="
+      --enable-perlinterp
+      --enable-pythoninterp
+      --enable-python3interp
+      --enable-rubyinterp
+      --enable-luainterp" ;;
+  small | tiny)
+    opt="" ;;
+esac
+
 cd vim && \
   ./configure \
   --with-features=$FEATURES \
-  --enable-perlinterp \
-  --enable-pythoninterp \
-  --enable-python3interp \
-  --enable-rubyinterp \
-  --enable-luainterp \
+  $opt \
   --enable-fail-if-missing && make || exit
 
 ./vim --version
